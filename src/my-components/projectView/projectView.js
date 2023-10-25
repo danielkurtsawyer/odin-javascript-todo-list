455bd1b2f8ab6df6532852d72eacfe82c20050de
danielkurtsawyer <19danielsawyer@gmail.com>
2023-10-25 13:37:19 -0400

    Finish to do item layout and creation

--- src/my-components/projectView/projectView.js
+++ src/my-components/projectView/projectView.js
@@ -1,6 +1,7 @@
 import EditIcon from './icons/edit.svg';
 import DeleteIcon from './icons/delete.svg';
 import MoveIcon from './icons/move.svg';
+import InfoIcon from './icons/info.svg';
 
 import Project from '../../project';
 import ToDoItem from '../../toDoItem';
@@ -17,6 +18,13 @@ export default (projectController, projectIndex = 0) => {
         projectView = document.createElement('div');
         projectView.classList.add('project-view');
     }
+
+    // at the top of the page, we can include a header for the Item title and Item due Date
+    // as well as provide a button to add items to the project 
+    const projectViewHeader = document.createElement('div');
+    projectViewHeader.classList.add('project-view-header');
+
+    
 
     // fetch the project object
     let project = projectController.getProject(projectIndex);
@@ -61,15 +69,29 @@ const createItemCard = (item, itemIndex) => {
     dueDate.textContent = format(item.dueDate, 'M/d/y');
     itemContainer.appendChild(dueDate);
 
-    const iconDelete = new Image();
-    iconDelete.src = DeleteIcon;
-    iconDelete.setAttribute('item-index', itemIndex);
-    itemContainer.appendChild(iconDelete);
+    const iconInfo = new Image();
+    iconInfo.src = InfoIcon;
+    iconInfo.setAttribute('item-index', itemIndex);
+    itemContainer.appendChild(iconInfo);
 
     const iconMove = new Image();
     iconMove.src = MoveIcon;
     iconMove.setAttribute('item-index', itemIndex);
     itemContainer.appendChild(iconMove);
-    
+
+    const iconDelete = new Image();
+    iconDelete.src = DeleteIcon;
+    iconDelete.setAttribute('item-index', itemIndex);
+    itemContainer.appendChild(iconDelete);
+
+
+    // add event listeners for hovering animation
+    iconInfo.addEventListener('mouseover', () => iconInfo.classList.add('hover'));
+    iconInfo.addEventListener('mouseout', () => iconInfo.classList.remove('hover'));
+    iconMove.addEventListener('mouseover', () => iconMove.classList.add('hover'));
+    iconMove.addEventListener('mouseout', () => iconMove.classList.remove('hover'));
+    iconDelete.addEventListener('mouseover', () => iconDelete.classList.add('hover'));
+    iconDelete.addEventListener('mouseout', () => iconDelete.classList.remove('hover'));
+
     return itemContainer;
 }
